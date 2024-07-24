0c73b423a56e2112ddb4a24165792d8a64d983a6
Akshita Sushil Bhatia <asb272@uowmail.edu.au>
2024-07-24 22:31:51 +0400

    update

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,17 +1,31 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Test = () => {
-    return (
-        <div className="course">
-            <motion.div 
-            className="box" 
-            initial={{opacity: 0.5, scale: 0.5}} 
-            transition={{duration: 2}}
-            whileInView={{opacity: 1, scale: 2}}
-            drag
-            ></motion.div>
-        </div>
-    );
+  const [open, setOpen] = useState(false);
+
+  const variants = {
+    visible: (i)=>( {
+      opacity: 1,
+      x: 100,
+      transition: { delay:i * 0.3 },
+    }),
+    hidden: { opacity: 0 },
+  };
+
+  const items = ["item1", "item2", "item3", "item4"];
+
+  return (
+    <div className="course">
+      <motion.ul initial="hidden" animate="visible" variants={variants}>
+        {items.map((item,i) => (
+          <motion.li variants={variants} key={item} custom={i}>
+            {item}
+          </motion.li>
+        ))}
+      </motion.ul>
+    </div>
+  );
 };
 
 export default Test;
